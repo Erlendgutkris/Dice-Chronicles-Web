c239983db2aec53ed8adaf2ca036b9e8949a740d
erlen <erlen@LENOVOERLEND>
2024-06-21 15:32:38 +0200

    refactored table to use json

--- api/routes/dice_battle_team_attacks.js
+++ api/routes/dice_battle_team_attacks.js
@@ -5,7 +5,17 @@ const path = require("path");
 const fs = require("fs");
 
 router.get("/", function (req, res, next){
-    res.render("dice_battle_team_attacks")
-})
+    var jsonFilePath = 'api/public/jsons/attacks.json';
+
+    fs.readFile(jsonFilePath, 'utf8', function(err, data) {
+      if (err) {
+        console.error('Error reading JSON file:', err);
+        return res.status(500).send('Error reading JSON file');
+      }
+  
+      var attackList = JSON.parse(data);
+    res.render("dice_battle_team_attacks", {attacks: attackList});
+});
+});
 
 module.exports = router;
